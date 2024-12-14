8701130280f34d3be5350c2a6706fdc5dc73af67
kim <m.absharhakim@gmail.com>
2024-12-14 21:49:16 +0700

    tulis apapun disini

---
+++ src/main/java/kelompok2/projectbp1/ProjectBP1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package kelompok2.projectbp1;
+
+/**
+ *
+ * @author WINDOWS 11
+ */
+public class ProjectBP1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
