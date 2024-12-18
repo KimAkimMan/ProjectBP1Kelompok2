01245d035b5db4377ce45e302162b580bb76b1a4
kim <m.absharhakim@gmail.com>
2024-12-18 11:41:36 +0700

    .

--- src/main/java/kelompok2/projectbp1/login.java
+++ src/main/java/kelompok2/projectbp1/login.java
@@ -150,7 +150,8 @@ private void login() {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
         login();
-
+halaman_utama hu = new halaman_utama();
+hu.setVisible(true);
         
     }//GEN-LAST:event_jButton1ActionPerformed
 
